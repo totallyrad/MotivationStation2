1b9a1512f9cead3ba3dc8b6b6ea9eab7c522c8a5
Ian K <ian.kawabata@gmail.com>
2017-06-01 11:53:54 -0700

    Update play.py

--- play.py
+++ play.py
@@ -9,8 +9,6 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
-
-GPIO.setup(27, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(17, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 os.system('mpg123 /home/system.mp3')
 while True:
